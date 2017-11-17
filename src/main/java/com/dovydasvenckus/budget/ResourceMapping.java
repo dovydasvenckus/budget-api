e04281a15c3cdcca411cd78c5df35a3013a28113
Dovydas Venckus <dovydas.venckus@4finance.com>
2017-11-17 13:18:18 +0200

    Externalizes base api url /api to separate variable

--- src/main/java/com/dovydasvenckus/budget/ResourceMapping.java
+++ src/main/java/com/dovydasvenckus/budget/ResourceMapping.java
@@ -1,6 +1,8 @@
 package com.dovydasvenckus.budget;
 
 public class ResourceMapping {
-    public static final String ACCOUNT_RESOURCE = "/api/accounts";
-    public static final String CLIENT_RESOURCE = "/api/clients";
+    private static final String BASE_URL = "/api/";
+
+    public static final String ACCOUNT_RESOURCE = BASE_URL + "accounts";
+    public static final String CLIENT_RESOURCE = BASE_URL + "clients";
 }
