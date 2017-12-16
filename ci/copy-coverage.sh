489e32ae398d08955a8f98ab5fec22926fe2e5c1
Dovydas Venckus <dovydas.venck@gmail.com>
2017-12-16 21:24:09 +0200

    Moved shippable bash scripts to separate directory
    Removed old CI scripts

--- ci/copy-coverage.sh
+++ ci/copy-coverage.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd $SHIPPABLE_BUILD_DIR
 mkdir -p shippable/codecoverage/target/site/jacoco
 cp build/reports/jacoco/test/jacocoTestReport.xml shippable/codecoverage/target/site/jacoco/jacoco.xml
 cp -r build/reports/jacoco/test/html shippable/codecoverage/target/site/jacoco/
--- ci/copy-test-results.sh
+++ ci/copy-test-results.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+cd $SHIPPABLE_BUILD_DIR
 mkdir -p shippable/testresults
 cp build/test-results/test/*.xml shippable/testresults
